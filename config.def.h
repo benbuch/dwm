bf648f603691e041fc8b3e8ae915af47316be1cb
Ben B <benjaminroaldbuch@icloud.com>
2021-01-23 13:42:01 +0300

    added hotkeys for moving(up/down) windows in the stack (dwm-push_no_master-6.2)

--- config.def.h
+++ config.def.h
@@ -108,6 +108,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
+	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
 	{ MODKEY,                       XK_n,      nametag,        {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
